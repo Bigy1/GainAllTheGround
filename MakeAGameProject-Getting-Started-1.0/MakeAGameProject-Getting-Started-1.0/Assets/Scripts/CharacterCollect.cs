7c6f2e6718824d20e409f98845e3837c42e505d9
Bigy1 <noahblough@gmail.com>
2018-07-18 17:48:40 -0400

    fixed some stuff

---
+++ MakeAGameProject-Getting-Started-1.0/MakeAGameProject-Getting-Started-1.0/Assets/Scripts/CharacterCollect.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterCollect : MonoBehaviour {
+
+    public Transform target;
+    public bool follow;
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	public void OnTriggerEnter2D() {
+        follow = true;
+	}
+
+    void Update()
+    {
+        if (follow)
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+        }
+    }
+}
